addb5645052f9585faa63b2ad15c462b1fc60301
Conan Truong <truong.conan@gmail.com>
2019-04-08 16:17:34 -0700

    Literally everything

---
+++ Save Data System Workshop/Assets/Scripts/SaveDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveDataManager : MonoBehaviour
+{
+    [SerializeField] GameObject spaghetti;
+    [SerializeField] InputField inputField;
+    [SerializeField] Dropdown dropdown;
+    [SerializeField] Slider slider;
+
+    public void Save()
+    {
+        // insert save code here
+    }
+
+    public void Load()
+    {
+        // insert load code here
+    }
+}
